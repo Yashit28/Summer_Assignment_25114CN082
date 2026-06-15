492b884bca9981c0380094ab20e80e41ce04cc8a
Yashit28 <yashitchauhan47@gmail.com>
2026-06-15 11:32:50 +0530

    Add files via upload

---
+++ day15/Q1.java
@@ -0,0 +1,28 @@
+// Q1 = Write a program to Reverse array. 
+
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+       int i = 0, j = n - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println("Reversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
---
+++ day15/Q3.java
@@ -0,0 +1,28 @@
+// Q3 = Write a program to Rotate array right. 
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the number of positions to rotate right: ");
+        int d = sc.nextInt();
+        d = d % n; // Handle cases where d is greater than n
+        int[] rotatedArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            rotatedArr[i] = arr[(i - d + n) % n];
+        }
+        System.out.println("Rotated array:");
+        for (int num = 0; num < rotatedArr.length; num++) {
+            System.out.print(rotatedArr[num] + " ");
+        }
+        sc.close();
+    }
+}
